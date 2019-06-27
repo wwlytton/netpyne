559d24e7db4f8cd43f9706a2dbba9119e2a7462e
Bill Lytton <neuro014@gmail.com>
2019-06-27 16:51:53 -0400

    finish 1st draft of runFromSavedState

--- netpyne/sim/run.py
+++ netpyne/sim/run.py
@@ -116,15 +116,15 @@ def runFromSavedState (file="state.sav"):
     sim.pc.barrier()
     sim.timing('start', 'runTime')
 
-    contTime = float(sim.cfg.duration) - time
-    if sim.rank == 0: print('\nRunning simulation for %g ms... up to %s'%(contTime,sim.cfg.duration))
-    sim.pc.psolve(sim.cfg.duration)
+    contTime = sim.cfg.duration - time
+    if sim.rank == 0: print('\nRunning simulation for %s ms... up to %s'%(contTime,sim.cfg.duration))
+    sim.pc.psolve(contTime)
 
     sim.pc.barrier() # Wait for all hosts to get to this point
     sim.timing('stop', 'runTime')
     if sim.rank==0:
         print('  Done; run time = %0.2f s; real-time ratio: %0.2f.' %
-            (sim.timingData['runTime'], sim.cfg.duration/1000/sim.timingData['runTime']))
+            (sim.timingData['runTime'], contTime/1000/sim.timingData['runTime']))
 
 #------------------------------------------------------------------------------
 # Run Simulation
